36e296af3facf9dfc77e0443d0ea58b3bd812558
Collin Van Dyck <collin@segment.com>
2020-04-09 09:59:19 -0400

    Initial commit

---
+++ main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"database/sql"
+	"flag"
+	"fmt"
+	"os"
+	"time"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func main() {
+	var (
+		host     = flag.String("host", "127.0.0.1", "")
+		port     = flag.Int("port", 3306, "")
+		user     = flag.String("user", "root", "")
+		password = flag.String("password", "pass", "")
+		sleep    = flag.Duration("sleep", time.Second, "")
+		timeout  = flag.Duration("timeout", 10*time.Second, "")
+	)
+	flag.Parse()
+	done := make(chan struct{})
+	go func() {
+		defer close(done)
+		for {
+			err := func() error {
+				dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/foo", *user, *password, *host, *port)
+				db, err := sql.Open("mysql", dsn)
+				if err != nil {
+					return err
+				}
+				err = db.Ping()
+				return err
+			}()
+			if err != nil {
+				fmt.Println(err)
+				time.Sleep(*sleep)
+				continue
+			}
+			return
+		}
+	}()
+
+	select {
+	case <-time.After(*timeout):
+		fmt.Println("mysql-wait: timed out trying to connect")
+		os.Exit(1)
+	case <-done:
+		fmt.Println("mysql-wait: connected")
+	}
+}
